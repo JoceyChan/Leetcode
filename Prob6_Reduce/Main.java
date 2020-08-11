85ff33bb371670150343c9bcc2b58704f5bdd194
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 23:29:48 -0700

    adding scanner and num

--- Prob6_Reduce/Main.java
+++ Prob6_Reduce/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        numberOfSteps(14);
+    }
+    public int numberOfSteps(int num) {
+        Scanner scan = new Scanner(System.in);
+        num = scan.nextInt();
     }
 }
