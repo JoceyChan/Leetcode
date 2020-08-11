a0298952e035268de11e864578b9bc04c0911d36
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 23:30:27 -0700

    space

--- Prob6_Reduce/Main.java
+++ Prob6_Reduce/Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        numberOfSteps(14);
     }
+    
     public int numberOfSteps(int num) {
         Scanner scan = new Scanner(System.in);
         num = scan.nextInt();
