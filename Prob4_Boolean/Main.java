52e8a69919fcf68de3abf7d8bf117498500f25a1
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 21:32:01 -0700

    candies[i] = maxCandies

--- Prob4_Boolean/Main.java
+++ Prob4_Boolean/Main.java
@@ -1,17 +1,16 @@
 import java.util.List;
 public class Main{
-    public static void main(String args[]){  
-        kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3);
-      
-    }
-    public static List <Boolean> kidsWithCandies(int[] candies, int extraCandies){
-
+    public static List <Boolean> main(int[] candies, int extraCandies){
+       
+    } 
+    public static List <Boolean> kidsWithCandies(int[] candies){
         int maxCandies = 0;
         for(int i = 0; i < candies.length; i++){
             System.out.println(candies[i]);
             if(candies[i] < maxCandies){
-                maxCandies = candies[i];
+                candies[i] = maxCandies;
             }               
         }
     }
+
 }
