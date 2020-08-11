7cd234cbbf6a084b8ce01c887868fcd691a9309e
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 23:20:26 -0700

    WIP

--- Prob4_Boolean/Main.java
+++ Prob4_Boolean/Main.java
@@ -1,16 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
 public class Main{
     public static List <Boolean> main(int[] candies, int extraCandies){
-       
+        kidsWithCandies(new int[]{2,3,5,1,3}, 3);
+
     } 
-    public static List <Boolean> kidsWithCandies(int[] candies){
-        int maxCandies = 0;
+    public static List <Boolean> kidsWithCandies(int[] candies, int extraCandies){
+       int maxCandies = 0;
         for(int i = 0; i < candies.length; i++){
-            System.out.println(candies[i]);
-            if(candies[i] < maxCandies){
-                candies[i] = maxCandies;
-            }               
+            if(candies[i] > maxCandies){
+                maxCandies = candies[i];
+                System.out.println(maxCandies);
+            } 
         }
+        return true;
     }
 
 }
