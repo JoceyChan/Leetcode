c1a3eec61545f9d572b889a3ec1c58609fcb65d6
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-11 11:37:08 -0700

    completed

--- Prob6_Reduce/Main.java
+++ Prob6_Reduce/Main.java
@@ -1,10 +1,27 @@
 import java.util.Scanner;
+import javax.print.attribute.standard.NumberOfDocuments;
+
 public class Main {
     public static void main(String[] args) {
-    }
-    
-    public int numberOfSteps(int num) {
         Scanner scan = new Scanner(System.in);
-        num = scan.nextInt();
-    }
-}
+        System.out.print("Enter a number: ");
+        int num = scan.nextInt();
+        int n = 0;
+
+        while( num != 0 ){
+            if( num % 2 == 0){
+                num = num / 2;
+                n += 1;
+            }
+                else {
+                    num = num - 1;
+                    n += 1;                
+                }
+                
+            }
+    System.out.println(n); 
+    scan.close();
+        } 
+    
+
+ }
