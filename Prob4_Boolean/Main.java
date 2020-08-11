dff32b9fdbb3c9f49b0f22742c42c8d905bb435c
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 21:16:42 -0700

    checking if candies = maxCandies

--- Prob4_Boolean/Main.java
+++ Prob4_Boolean/Main.java
@@ -6,12 +6,12 @@ public class Main{
     }
     public static List <Boolean> kidsWithCandies(int[] candies, int extraCandies){
 
+        int maxCandies = 0;
         for(int i = 0; i < candies.length; i++){
             System.out.println(candies[i]);
-            if(candies[i] < extraCandies){
-                System.out.println("false");
+            if(candies[i] < maxCandies){
+                maxCandies = candies[i];
             }               
         }
-        return false;
     }
 }
