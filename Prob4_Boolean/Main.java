11302133243420f666b2c64c4be145fc26b5ddf7
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 21:10:52 -0700

    creating a method and calling method

--- Prob4_Boolean/Main.java
+++ Prob4_Boolean/Main.java
@@ -1,12 +1,17 @@
+import java.util.List;
 public class Main{
-    public static void main(String[] args){
-        
-        int[] candies = {2, 3, 5, 1, 3};
-        int extraCandies = 3;
+    public static void main(String args[]){  
+        kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3);
+      
+    }
+    public static List <Boolean> kidsWithCandies(int[] candies, int extraCandies){
 
-        for(int i = 0; i < 6; i++){
+        for(int i = 0; i < candies.length; i++){
             System.out.println(candies[i]);
+            if(candies[i] < extraCandies){
+                System.out.println("false");
+            }               
         }
-
+        return false;
     }
 }
