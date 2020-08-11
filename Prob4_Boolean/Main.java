1964b6619ea221544ee38831de149d35e032b8c7
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 20:37:32 -0700

    printing array

--- Prob4_Boolean/Main.java
+++ Prob4_Boolean/Main.java
@@ -4,5 +4,9 @@ public class Main{
         int[] candies = {2, 3, 5, 1, 3};
         int extraCandies = 3;
 
+        for(int i = 0; i < 6; i++){
+            System.out.println(candies[i]);
+        }
+
     }
 }
