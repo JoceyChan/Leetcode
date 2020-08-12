b144c328d976fdfdb9f0d1c706f53e5875a86b3c
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-12 11:53:40 -0700

    printing and scanning

---
+++ Prob7_Distinction/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public int numJewelsInStones(String J, String S) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("J = ");
+        String j = scan.nextLine();
+
+        System.out.print("S = ");
+        String s = scan.nextLine();
+    }
+}
