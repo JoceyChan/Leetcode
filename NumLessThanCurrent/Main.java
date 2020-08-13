52577c062a8ef22448aed126006202bc6d2f6090
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-13 15:15:21 -0700

    for loop prints out array index

--- NumLessThanCurrent/Main.java
+++ NumLessThanCurrent/Main.java
@@ -4,6 +4,10 @@ public class Main {
         smallerNumbersThanCurrent(new int[]{8,1,2,2,3});
     }
     public static int[] smallerNumbersThanCurrent(int[] nums) {
-        
+        int[] arr = new int[]{};
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+        return nums;
     }
 }
