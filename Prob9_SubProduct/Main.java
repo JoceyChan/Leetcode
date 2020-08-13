dd12440c751f5b0ecae71e15ded5a1727642e30a
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-12 21:07:21 -0700

    renaming signature method

--- Prob9_SubProduct/Main.java
+++ Prob9_SubProduct/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         
     }
-    public int numJewelsInStones(String J, String S) {
+    public static int subtractProductAndSum(int n) {
         
     }
 }
