06777fdfa05dfbf3f6d8f3e40b16214645e1764f
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-12 23:57:02 -0700

    getting the product and sum of the digits

--- Prob9_SubProduct/Main.java
+++ Prob9_SubProduct/Main.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        subtractProductAndSum(234);
+        System.out.println(subtractProductAndSum(234));
     }
     public static int subtractProductAndSum(int n) {
-        int mod = 0;
+        int mod;
+        int product = 1;
+        int sum = 0;
+        int result = 0;
 
         while (n > 0){
-            mod = n % 10; // mod is = to the division of n by 10 and gets the remainder 
+            mod = (n % 10); // mod is = to the division of n by 10 and gets the remainder 
+            product = product * mod;
+            sum = sum + mod;
             n = n/10; // n is being reassigned, n is being divided by 10 and then goes back to the while loop
-            System.out.println(mod); //prints the remainder of mod
+            // System.out.println(mod); //prints the remainder of mod
         }
-        return mod;
+        return product;
     }
 }
