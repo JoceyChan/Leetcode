e0f88f2fb2557005a532f7388fa331957a21549b
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-12 22:10:42 -0700

    prints the remainder of mod

--- Prob9_SubProduct/Main.java
+++ Prob9_SubProduct/Main.java
@@ -9,7 +9,7 @@ public class Main {
         while (n > 0){
             mod = n % 10; // mod is = to the division of n by 10 and gets the remainder 
             n = n/10; // n is being reassigned, n is being divided by 10 and then goes back to the while loop
-            System.out.println(mod); //prints the remander of mod
+            System.out.println(mod); //prints the remainder of mod
         }
         return mod;
     }
