1c330d68b14a4dfffdbcd9c7707c215cba077312
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-12 22:08:51 -0700

    separtaing the number into digits

--- Prob9_SubProduct/Main.java
+++ Prob9_SubProduct/Main.java
@@ -1,8 +1,16 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        
+        subtractProductAndSum(234);
     }
     public static int subtractProductAndSum(int n) {
-        
+        int mod = 0;
+
+        while (n > 0){
+            mod = n % 10; // mod is = to the division of n by 10 and gets the remainder 
+            n = n/10; // n is being reassigned, n is being divided by 10 and then goes back to the while loop
+            System.out.println(mod); //prints the remander of mod
+        }
+        return mod;
     }
 }
