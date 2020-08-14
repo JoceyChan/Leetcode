e9782b58346fe363edc2458f01d4df638fb82ac2
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-14 00:25:53 -0700

    completed

--- NumLessThanCurrent/Main.java
+++ NumLessThanCurrent/Main.java
@@ -1,16 +1,24 @@
+import java.util.*;
+
 public class Main {
     
     public static void main(String[] args){
         smallerNumbersThanCurrent(new int[]{8,1,2,2,3});
     }
     public static int[] smallerNumbersThanCurrent(int[] nums) {
-        int i;
-        int[] pairs = new int[]{};
+        int count = 0;
+        int[] newArrayList = new int[nums.length];
 
-        for (i = 0; i < nums.length; i++) { 
-
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(nums[j] !=  nums[i] && nums[i] > nums[j]){
+                    count++;
+                }
+            }
+            newArrayList[i] = count;
+            count = 0;
         }
-
-        return nums;
+        System.out.println(Arrays.toString(newArrayList));
+        return newArrayList;
     }
 }
