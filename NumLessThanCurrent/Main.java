d9780d99b5f6b83cc3116b1eef54f0bb3b7b6ea4
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-13 20:34:50 -0700

    created a for loop and a new array

--- NumLessThanCurrent/Main.java
+++ NumLessThanCurrent/Main.java
@@ -4,10 +4,13 @@ public class Main {
         smallerNumbersThanCurrent(new int[]{8,1,2,2,3});
     }
     public static int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] arr = new int[]{};
-        for(int i = 0; i < nums.length; i++){
-            System.out.println(nums[i]);
+        int i;
+        int[] pairs = new int[]{};
+
+        for (i = 0; i < nums.length; i++) { 
+
         }
+
         return nums;
     }
 }
