5efa03388cab452f555c72c03ee112a9ad3994ec
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-15 10:43:28 -0700

    Completed

--- Prob7_Distinction/Main.java
+++ Prob7_Distinction/Main.java
@@ -1,21 +1,22 @@
-package Prob7_Distinction;
 import java.util.Scanner;
 
 public class Main {
-    
     public static void main(String[] args) {
-        
+        numJewelsInStones("aA", "aAAbbbb"); //Calling method
+        numJewelsInStones("z", "ZZ");
     }
-    public int numJewelsInStones(String J, String S) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("J = ");
-        String j = scan.nextLine();
+    
+    public static int numJewelsInStones(String J, String S) {
+     int count = 0;
 
-        System.out.print("S = ");
-        String s = scan.nextLine();
-
-        for(int i = 0; i < s.length(); i++){
-            System.out.println();
+        for(int i = 0; i < J.length(); i++){ 
+            for(int j = 0; j < S.length(); j++){
+                if(J.charAt(i) == S.charAt(j)){ //checking if both each index of the strings are equal 
+                    count++; // incrementing 
+                }
+            }
         }
+        System.out.println(count); 
+        return count;
     }
 }
