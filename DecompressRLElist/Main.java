010eda6cde80d3251748fe49059a0af256200ee2
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-15 13:41:39 -0700

    Crested main method, called method

---
+++ DecompressRLElist/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        decompressRLElist(new int[]{1,2,3,4});
+    }
+    public static int[] decompressRLElist(int[] nums) {
+        
+    }
+}
