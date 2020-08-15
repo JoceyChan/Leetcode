226eed9287ee4d7f95baaa6077d05683889b5feb
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-15 13:24:28 -0700

    Completed

--- Prob4_Boolean/Main.java
+++ Prob4_Boolean/Main.java
@@ -1,17 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Main{
     public static void main(String[] args){
         kidsWithCandies(new int[]{2,3,5,1,3}, 3);
 
     } 
-    public static List <Boolean> kidsWithCandies(int[] candies, int extraCandies){
-       int maxCandies = 0;
+    public static List <Boolean> kidsWithCandies(int[] candies, int extraCandies){       
+        ArrayList<Boolean> output = new ArrayList<Boolean>();
+        boolean isGreaterThanTheRest = true;
+
         for(int i = 0; i < candies.length; i++){
             for(int j = 0; j < candies.length; j++){
+                if(candies[i] + extraCandies < candies[j]){
+                    isGreaterThanTheRest = false;
+                } 
             }
-        }
-        return candies;
+            output.add(isGreaterThanTheRest);
+            isGreaterThanTheRest = true;
+        } 
+        System.out.println(output);
+        return output;
     }
-
 }
