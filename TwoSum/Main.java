65ae9c875b7a6703f243dd43df22f07dc6a810c8
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-16 16:56:25 -0700

    Time and Space

--- TwoSum/Main.java
+++ TwoSum/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args){
         twoSum(new int[]{2, 7, 11, 15}, 9);
     }
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(1)
     public static int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
         
