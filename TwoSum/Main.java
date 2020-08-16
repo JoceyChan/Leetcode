2b3a6a3fa17681367ae64a89c26b545eb3a3f820
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-16 16:45:21 -0700

    completed

--- TwoSum/Main.java
+++ TwoSum/Main.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         twoSum(new int[]{2, 7, 11, 15}, 9);
     }
     public static int[] twoSum(int[] nums, int target) {
+        int[] indices = new int[2];
         
+        for (int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(nums[i] + nums[j] == target && nums[j] != nums[i]){
+                    indices[0] = i;
+                    indices[1] = j;
+                    return indices;
+                }
+            }
+        }    
+        return new int[]{-1, -1};
     }
 }
