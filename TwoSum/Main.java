a585c04517ecb78661ced119658b3c1cd25cc917
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-16 14:59:38 -0700

    created twoSum method and called it in main method

--- TwoSum/Main.java
+++ TwoSum/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
+        twoSum(new int[]{2, 7, 11, 15}, 9);
+    }
+    public static int[] twoSum(int[] nums, int target) {
         
     }
 }
