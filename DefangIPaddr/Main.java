b8ec548307248a7f3e1182b9c740727a55b37254
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-15 18:18:06 -0700

    made method static

--- DefangIPaddr/Main.java
+++ DefangIPaddr/Main.java
@@ -2,16 +2,9 @@ public class Main {
     public static void main(String[] args){
         defangIPaddr("1.1.1.1");
     }
-    public static String defangIPaddr(String address) {
 
-        for(int i = 0; i < address.length(); i++){
-            // System.out.println(address.charAt(i));
-            int index = address.indexOf('.');
-            if(address.charAt(i) == index){
-                System.out.println(index);
-            }
-            System.out.println(index);
-        }
+    public static String defangIPaddr(String address) {
+        address = address.replace(".", "[.]");
         return address;
     }
 }
