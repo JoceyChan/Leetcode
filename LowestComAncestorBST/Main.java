4e8890d81e7b12632565f90c8bc849a9eea96f82
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-17 13:52:24 -0700

    WIP

---
+++ LowestComAncestorBST/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.tree.TreeNode;
+
+public class Main {
+    public static void main(String[] args) {
+        lowestCommonAncestor("6,2,8,0,4,7,9,null,null,3,5", 2, 8);
+        
+    }
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int left = 0; 
+        int right = list.length -1;
+    }
+}
