7eaeaade542c598280d1324054ce8feca778e549
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-17 11:39:28 -0700

    completed

--- MaximumSubarray/Main.java
+++ MaximumSubarray/Main.java
@@ -3,18 +3,16 @@ public class Main {
         maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
     }
     public static int maxSubArray(int[] nums) { 
-        // int max = Integer.MIN_VALUE;
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         int currentMaxSum = 0;
-        // System.out.println(Integer.MIN_VALUE);
-
+        
         for(int i = 0; i < nums.length; i++){
             currentMaxSum += nums[i];
+            if(max < currentMaxSum){
+                max = currentMaxSum;
+            }
             if (currentMaxSum < 0){
                 currentMaxSum = 0;
-            }
-            else if(max < currentMaxSum){
-                max = currentMaxSum;
             }
         }
     System.out.println(max);
