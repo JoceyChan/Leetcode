7459981ebc56b94a49fd9f28b3a95dc74278531d
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-17 11:33:19 -0700

    completed

---
+++ MaximumSubarray/DifferentWay.java
@@ -0,0 +1,22 @@
+public class DifferentWay {
+    public static void main(String[] args) {
+        maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+    }
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int currentMaxSum = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            currentMaxSum += nums[i];
+            // System.out.println(currentMaxSum);
+            if(max < currentMaxSum){
+                max = currentMaxSum;
+            }
+            if (currentMaxSum < 0){
+                currentMaxSum = 0;
+            }
+        }
+    System.out.println(max);
+    return max;
+    }
+}
