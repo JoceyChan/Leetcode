d2ee8d4e127313e7ce202d9bd745d019f72ce90a
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-16 17:00:56 -0700

    calling method and making method static

--- RemoveDuplicates/Main.java
+++ RemoveDuplicates/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     
     public static void main(String[] args){
-
+        removeDuplicates(new int[]{1, 1, 2});
     }
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
         
     }
 }
