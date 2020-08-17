42c27d1a8d9d0a48ef1473355fa1e6321bb399b4
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-16 19:11:58 -0700

    iterating through the string

--- LengthOfLongestSubstring/Main.java
+++ LengthOfLongestSubstring/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args){
         lengthOfLongestSubstring("abcabcbb");
-        lengthOfLongestSubstring("bbbbb");
-        lengthOfLongestSubstring("pwwkew");
+        // lengthOfLongestSubstring("bbbbb");
+        // lengthOfLongestSubstring("pwwkew");
     }
+    //public boolean contains  (String str)
     public static int lengthOfLongestSubstring(String s) {
-        
-    }
-}
+        int count = 0;
+        char[] dup = s.toCharArray();
+
+        for(int i = 0; i < s.length(); i++){
+            System.out.println(dup[i]);     
+        }
+    return 0;
+}}
