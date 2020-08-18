0b9343c8f2a1f453b68104c95d13025c50c127d4
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-18 14:33:30 -0700

    Time and S

--- SortColors/Main.java
+++ SortColors/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args){
         sortColors(new int[]{2, 0, 2, 1, 1, 0});
     }
+    //Time Complexity: O(n)
+    //Space Complexity: O(1)
     public static void sortColors(int[] nums) {
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
