d44603e5cfb2e837d73f0d0a343b602c252c76ea
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-18 12:42:47 -0700

    calling sortColors method and iterating

--- SortColors/Main.java
+++ SortColors/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args){
-
+        sortColors(new int[] {2,0,2,1,1,0});
     }
-    public void sortColors(int[] nums) {
-        
+    public static void sortColors(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 1; j < nums.length; i++){
+                if(nums[i] < nums[j]){
+                    System.out.println(nums[i]);
+                }
+            }
+        }
     }
-}
 }
