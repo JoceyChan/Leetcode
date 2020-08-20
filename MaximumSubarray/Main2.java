9c6052d70729df9e0f7740e54783152ae03063c8
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-20 14:10:37 -0700

    WIP

---
+++ MaximumSubarray/Main2.java
@@ -0,0 +1,20 @@
+public class Main2 {
+    public static void main(String[] args) {
+        maxSubArray(new int[]{5, 7, 5, 4, 5, 8, 2});
+    }
+    public static int maxSubArray(int[] n, int k){
+        // int max = Integer.MIN_VALUE;
+        // int currentMaxSum = 0;
+        for(int i = 0; i < n.length; i++){
+        //     currentMaxSum += arr[i];
+        //     if(max < currentMaxSum){
+        //         max = currentMaxSum;
+        //     }
+        //     if (currentMaxSum < 0){
+        //         currentMaxSum = 0;
+        //     }
+        }
+    // System.out.println(max);
+    // return max;
+    }
+}
