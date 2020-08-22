485f3c692bee4a97a1615a02bc8872507cde4a8a
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-22 14:35:08 -0700

    WIP

--- SingleNumber/Main.java
+++ SingleNumber/Main.java
@@ -1,10 +1,11 @@
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // singleNumber(new int[]{2, 2, 1});
-        singleNumber(new int[]{4, 1, 2, 1, 2});
+        // singleNumber(new int[]{4, 1, 2, 1, 2});
     }
     public static int singleNumber(int[] nums){
         HashMap<Integer, Integer> hashTable = new HashMap<Integer, Integer>();//creates HashMap
@@ -17,5 +18,14 @@ public class Main {
                 hashTable.put(nums[i], hashTable.get(nums[i])+ 1);
             }
         }
+        for(Map.Entry<Integer, Integer> entry :  hashTable.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            if(value == 1){
+                return key;
+            }
+        }
+        return -1;
     }
 }
