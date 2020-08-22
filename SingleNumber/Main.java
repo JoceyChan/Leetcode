c6f5c32fb309e07aa6d292ea462f97f3dc15c921
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-21 18:03:43 -0700

    Initalizing hashmap

--- SingleNumber/Main.java
+++ SingleNumber/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +7,15 @@ public class Main {
         singleNumber(new int[]{4, 1, 2, 1, 2});
     }
     public static int singleNumber(int[] nums){
-        HashMap<Integer, Integer> hash_table = new HashMap<Integer, Integer>();//creates HashMap
+        HashMap<Integer, Integer> hashTable = new HashMap<Integer, Integer>();//creates HashMap
+
+        for(int i = 0; i < nums.length; i++){
+            if(!hashTable.containsKey(nums[i])){ 
+                hashTable.put(nums[i], 1);
+            }
+            else{
+                hashTable.put(nums[i], hashTable.get(nums[i])+ 1);
+            }
+        }
     }
 }
