07bc122aef9af581d0e8b2a1fe91160913490093
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-22 16:09:55 -0700

    created a char[] and two variables

--- LengthOfLongestSubstring/Main.java
+++ LengthOfLongestSubstring/Main.java
@@ -6,9 +6,9 @@ public class Main {
     }
     //public boolean contains  (String str)
     public static int lengthOfLongestSubstring(String s) {
-        char[] letter = new char[s.length()];
-        for(int i = 0; i < s.length(); i++){
-           for(int j = i+1; j < s.length(); j++){} 
-        }
+        char[] input = s.toCharArray();
+        
+        String longestSubstring = null;
+        int longestSubstrengthSize = 0;
     }
 } 
