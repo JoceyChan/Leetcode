cad62f63fbbc293363a92a468fa786e5e0acf1d4
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-21 17:27:50 -0700

    created hashMap

--- SingleNumber/Main.java
+++ SingleNumber/Main.java
@@ -1,8 +1,10 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // singleNumber(new int[]{2, 2, 1});
+        singleNumber(new int[]{4, 1, 2, 1, 2});
     }
     public static int singleNumber(int[] nums){
-
+        HashMap<Integer, Integer> hash_table = new HashMap<Integer, Integer>();//creates HashMap
     }
-}
