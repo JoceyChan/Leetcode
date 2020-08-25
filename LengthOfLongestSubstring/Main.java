c251ab1aa1583b59bf58f3fd9412d235b4453fb3
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-24 19:12:38 -0700

    WIP

--- LengthOfLongestSubstring/Main.java
+++ LengthOfLongestSubstring/Main.java
@@ -1,14 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args){         
         lengthOfLongestSubstring("abcabcbb");
         // lengthOfLongestSubstring("bbbbb");
         // lengthOfLongestSubstring("pwwkew");
     }
     //public boolean contains  (String str)
     public static int lengthOfLongestSubstring(String s) {
-        char[] input = s.toCharArray();
-        
-        String longestSubstring = null;
-        int longestSubstrengthSize = 0;
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++)
+            for (int j = i + 1; j <= s.length(); j++)
+                if (allUnique(s, i, j)) {
+                    ans = Math.max(ans, j - i);
+                }
+                return ans;
+            }
+    
+    public static boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+                set.add(ch);
+        }
+    return true;
     }
-} 
+    
+}
