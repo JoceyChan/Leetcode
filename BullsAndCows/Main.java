6d89da6a6ffa15a4eb16ae9b0b471ed8396ae15c
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-29 12:10:56 -0700

    WIP

--- BullsAndCows/Main.java
+++ BullsAndCows/Main.java
@@ -6,18 +6,31 @@ public class Main {
     public static String getHint(String secret, String guess){
         HashMap<Character, Integer> A = new HashMap<Character, Integer>();
         HashMap<Character, Integer> B = new HashMap<Character, Integer>();
-        
+
         int count = 0;
         for(int i = 0; i < secret.length(); i++){
             if(secret.charAt(i) == guess.charAt(i)){ 
-                // System.out.println(secret.charAt(i) + "equal" + guess.charAt(i));
-                System.out.println("A");
                 count++;
             }
-            else if(secret.charAt(i) != guess.charAt(i)){
-                System.out.println("B");
+            else if(A.containsKey(secret.charAt(i))){
+                int temp = A.get(secret.charAt(i));
+                temp++; // incrementing the freq by one 
+                A.put(secret.charAt(i), temp); // updating the hashmap A
+            }
+            else if(!A.containsKey(secret.charAt(i))){
+                A.put(secret.charAt(i), 1);
+            }
+            else if(B.containsKey(guess.charAt(i))){
+                int temp = B.get(guess.charAt(i));
+                temp++; // incrementing the freq by one 
+                B.put(guess.charAt(i), temp); // updating the hashmap B
+            }
+            else if(!B.containsKey(guess.charAt(i))){
+                B.put(guess.charAt(i), 1);
             }
         }
+        // System.out.println(A);
+        // System.out.println(B);
         return secret;
     }
 }
