8eb7fc0c92a497ee55dabd40b93b6d01aca32b20
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-29 11:11:11 -0700

    comparing

--- BullsAndCows/Main.java
+++ BullsAndCows/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
     }
     public static String getHint(String secret, String guess){
-        
+        for(int i = 0; i < secret.length(); i++){
+            for(int j = 0; j < guess.length(); j++){
+                if(secret.charAt(i) == guess.charAt(j)){
+                    System.out.println("equal");
+                }
+            }
+        }
     }
 }
