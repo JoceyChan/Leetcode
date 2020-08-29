a09ebd93f5d94ab53b353a58a698077984ae6ed2
Jocey Chan <joceychan@Franciscos-MacBook-Pro.local>
2020-08-29 11:28:48 -0700

    Import HashMap, created 2 maps

--- BullsAndCows/Main.java
+++ BullsAndCows/Main.java
@@ -1,8 +1,12 @@
+import java.util.HashMap;
 public class Main {
     public static void main(String[] args){
         getHint("1807", "7810");
     }
     public static String getHint(String secret, String guess){
+        HashMap<Character, Integer> A = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> B = new HashMap<Character, Integer>();
+        
         int count = 0;
         for(int i = 0; i < secret.length(); i++){
             if(secret.charAt(i) == guess.charAt(i)){ 
