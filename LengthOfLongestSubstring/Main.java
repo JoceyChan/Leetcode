8772cbc1a36519120fea91a9553f45af8bf5e28e
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-29 17:55:27 -0700

    O(n)

--- LengthOfLongestSubstring/Main.java
+++ LengthOfLongestSubstring/Main.java
@@ -8,24 +8,20 @@ public class Main {
         // lengthOfLongestSubstring("pwwkew");
     }
     //public boolean contains  (String str)
-    public static int lengthOfLongestSubstring(String s) {
-        int ans = 0;
-        for (int i = 0; i < s.length(); i++)
-            for (int j = i + 1; j <= s.length(); j++)
-                if (allUnique(s, i, j)) {
-                    ans = Math.max(ans, j - i);
-                }
-                return ans;
-            }
-    
-    public static boolean allUnique(String s, int start, int end) {
+    public  static int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
-        for (int i = start; i < end; i++) {
-            Character ch = s.charAt(i);
-            if (set.contains(ch)) return false;
-                set.add(ch);
+
+        int ans = 0, i = 0, j = 0;
+        while(i < s.length() && j < s.length()){
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else{
+                set.remove(s.charAt(i++));
+            }
         }
-    return true;
+        return ans;
     }
-    
+
 }
