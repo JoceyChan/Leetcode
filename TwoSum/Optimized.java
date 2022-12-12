a7330b7191cf36cd375d985237d3e352550c19fd
JoceyChan <joceychan331718@gmail.com>
2022-12-12 15:21:12 -0800

    Set Up Methods

---
+++ TwoSum/Optimized.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+public class Optimized {
+    public static void main(String[] args){
+        twoSum(new int[]{2, 7, 11, 15}, 9);
+    }
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(1)
+    public static int[] twoSum(int[] nums, int target) {
+        
+    }
+}
