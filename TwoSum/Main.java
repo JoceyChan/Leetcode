2636be8ac38838eea5349559ece58d4ba8d06a16
JoceyChan <joceychan331718@gmail.com>
2022-12-12 15:57:33 -0800

    Optimized Version

--- TwoSum/Main.java
+++ TwoSum/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args){
@@ -6,18 +6,33 @@ public class Main {
     }
     // Time Complexity: O(n^2)
     // Space Complexity: O(1)
-    public static int[] twoSum(int[] nums, int target) {
-        int[] indices = new int[2];
+    // public static int[] twoSum(int[] nums, int target) {
+    //     int[] indices = new int[2];
         
-        for (int i = 0; i < nums.length; i++){
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i] + nums[j] == target && j != i){
-                    indices[0] = i;
-                    indices[1] = j;
-                    return indices;
-                }
+    //     for (int i = 0; i < nums.length; i++){
+    //         for(int j = 0; j < nums.length; j++){
+    //             if(nums[i] + nums[j] == target && j != i){
+    //                 indices[0] = i;
+    //                 indices[1] = j;
+    //                 return indices;
+    //             }
+    //         }
+    //     }    
+    //     return new int[]{-1, -1}; // can also just return null
+    // }
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int sub =  target - nums[i];
+            if(map.containsKey(sub) && map.get(sub) != i ){
+               int[] resp = new int[]{i, map.get(sub)};
+               return resp;
             }
-        }    
-        return new int[]{-1, -1}; // can also just return null
+        }
+        return null;
     }
 }
