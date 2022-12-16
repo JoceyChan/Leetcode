b568d1d4f172ab435d5a8fad1453eff66728f749
JoceyChan <joceychan331718@gmail.com>
2022-12-16 13:57:52 -0800

    4Sum WIP

---
+++ 4Sum/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        fourSum(new ArrayList<>(1,0,-1,0,-2,2), 0);
+    }
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        
+    }
+}
