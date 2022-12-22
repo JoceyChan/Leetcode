5e6ba7454da79ec5d16fdeec0d453617da2ce0c5
JoceyChan <joceychan331718@gmail.com>
2022-12-22 15:17:39 -0800

    WIP

--- 3Sum/Main.java
+++ 3Sum/Main.java
@@ -1,7 +1,20 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        fourSum(new ArrayList<>(1,0,-1,0,-2,2), 0);
+        threeSum(new List<>(-1,0,1,2,-1,-4));
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        // List<List<Integer>> threeSum = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            int j = i + 1; 
+            int k = nums.length + 1;
+            if(i != j && i != k && j != k){
+                System.out.println("no dups");
+            }
+        }
+        return null;
     }
 }
