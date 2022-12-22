9e9368bb1b28d74b552326c1f94202c96a723a22
JoceyChan <joceychan331718@gmail.com>
2022-12-22 15:43:38 -0800

    Brute Force

--- ContainsDups/Main.java
+++ ContainsDups/Main.java
@@ -7,14 +7,11 @@ public class Main {
 
     public static boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            System.out.println(nums[i]);
+        for(int i = 0; i < nums.length - 1; i++){
             if(nums[i] == nums[i+1]){
-                System.out.println("true");
                 return true;
             }
         }
-        System.out.println("false");
         return false;
     }
 }
