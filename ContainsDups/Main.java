0292445d4a5c4d59cbbe822731f9e8bce2de5dee
JoceyChan <joceychan331718@gmail.com>
2022-12-22 15:34:26 -0800

    WIP

--- ContainsDups/Main.java
+++ ContainsDups/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         containsDuplicate(new int[]{1, 2, 3, 1});
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+            if(nums[i] == nums[i+1]){
+                System.out.println("true");
+                return true;
+            }
+        }
+        System.out.println("false");
+        return false;
     }
 }
