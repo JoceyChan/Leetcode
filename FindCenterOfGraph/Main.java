eee591d998094b2c52cd7e0e26e47b4005a91c3a
JoceyChan <joceychan331718@gmail.com>
2023-02-21 15:31:55 -0800

    Created folder for problem

---
+++ FindCenterOfGraph/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        findCenter(new int{[[1,2],[2,3],[4,2]]});
+    }
+
+    public int findCenter(int[][] edges) {
+        
+    }
+}
