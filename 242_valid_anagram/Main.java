7950a90962e44f1f7b2b650c7303e26f853b8c35
JoceyChan <joceychan331718@gmail.com>
2024-09-21 18:00:01 -0700

    calling isAnagram methid

--- 242_valid_anagram/Main.java
+++ 242_valid_anagram/Main.java
@@ -9,9 +9,9 @@
 
 public class Main {
     public static void main(String args[]){
-
+        isAnagram(anagram, nagaram);
     }
     public boolean isAnagram(String s, String t) {
-
+        
     }
 }
