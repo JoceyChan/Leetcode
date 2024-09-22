320705e30b5df679072aa41c5bd15f908f5549bc
JoceyChan <joceychan331718@gmail.com>
2024-09-21 17:59:02 -0700

    problem description

--- 242_valid_anagram/Main.java
+++ 242_valid_anagram/Main.java
@@ -1,8 +1,17 @@
+/*  Given two strings s and t, return true if t is an anagram of s, and false otherwise
+        Ex 1: 
+            input: s = "anagram" t = "nagaram" 
+            output: true
+        Ex 2: 
+            input: s = "rat" t = "car" 
+            output: false
+*/
+
 public class Main {
     public static void main(String args[]){
 
     }
     public boolean isAnagram(String s, String t) {
-        
+
     }
 }
