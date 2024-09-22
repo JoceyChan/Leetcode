c5f16355045bd69e428346fbccdceb1877ba172f
JoceyChan <joceychan331718@gmail.com>
2024-09-22 16:21:26 -0700

    solved

--- 242_valid_anagram/Main.java
+++ 242_valid_anagram/Main.java
@@ -6,13 +6,37 @@
             input: s = "rat" t = "car" 
             output: false
 */
+import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]){
         isAnagram("anagram", "nagaram");
+        isAnagram("rat", "car");
+        isAnagram("ab", "a");
     }
     public static boolean isAnagram(String s, String t) {
-        System.out.println(s);
-        return false;
+
+        if(s.length() != t.length()){
+            System.out.println("false");
+            return false;
+        }
+
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        for(int i = 0; i < str1.length; i++){
+            for(int j = 0; j < str2.length; j++){
+                if(str1[i] != str2[j]){
+                    System.out.println("false");
+                    return false;
+                }
+                i++;
+            }
+        }
+        System.out.println("true");
+        return true;
     }
 }
