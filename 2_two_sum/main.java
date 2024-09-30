d58b4fcad8dc24886103d66e69a87d19f0230dc1
JoceyChan <joceychan331718@gmail.com>
2024-09-30 14:08:47 -0700

    setting up method

---
+++ 2_two_sum/main.java
@@ -0,0 +1,12 @@
+import java.util.arrays;
+
+public class Main {
+    public static void main(String args[]){
+        int[] nums = {2, 4, 11, 15};
+        twoSum(nums, 9);
+    }
+
+    public static int[] twoSum(inr[] nums, int target){
+        
+    }
+}
