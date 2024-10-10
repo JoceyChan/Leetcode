53cee98f85b711df131861ae9e3e81ec5e2ec40e
JoceyChan <joceychan331718@gmail.com>
2024-10-09 18:40:39 -0700

    Problem 217 Contains Duplicate Set up

---
+++ 217_contains_duplicate/Main.java
@@ -0,0 +1,22 @@
+/* 217. Contains Duplicate
+    * Easy
+    * Given an integer array nums, return true if any value appeards at least twice in the array, 
+    * and return false if evey element is distinct
+    * 
+    *  Example 1: 
+    * 
+    *          Input: nums = [1, 2, 3, 1]
+    *          Output: true
+    * 
+    *  Example 2: 
+    * 
+    *          Input: nums[1, 2, 3, 4]
+    *          Output: false
+ */
+
+
+public class Main {
+    public static void main(String[] args){
+        containsDuplicate();
+    }
+}
